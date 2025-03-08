7311a9d0f12abca3158927c6e54055cfb4b8a661
mateustegue <teusteguetorres2001@gmail.com>
2025-03-08 13:34:51 -0500

    cambio de la cantidad de caracter para las cards

--- frontend/src/components/TaskCard.jsx
+++ frontend/src/components/TaskCard.jsx
@@ -8,8 +8,8 @@ export function TaskCard({ task }){
         
 
         key={task.id_tarea}>
-            <h1 className="font-bold text-2xl m-4">{task.titulo.length > 20 ? task.titulo.substring(0 , 15 ) + "..." : task.titulo}</h1>
-            <p className="text-gray-300 text-lg m-4">{task.descripcion.length > 10 ? task.descripcion.substring(0, 40) + "..." : task.descripcion}</p>
+            <h1 className="font-bold text-2xl m-4">{task.titulo.length > 7 ? task.titulo.substring(0 , 7 ) + "..." : task.titulo}</h1>
+            <p className="text-gray-300 text-lg m-4">{task.descripcion.length > 7 ? task.descripcion.substring(0, 10) + "..." : task.descripcion}</p>
             <p className="text-red-500 font-bold m-4">{task.estado}</p>
             <Link to={`/taskDetial/${task.id_tarea}`} className="block  m-4  bg-blue-500 text-white font-semibold py-2 rounded-lg text-center hover:bg-blue-600 transition">Leer...</Link>
          </div>
